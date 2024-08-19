4ff8bcf4f1004a06499fdf97a964fc53c7d036e6
AndrewCogi <whgustlr0326@gmail.com>
2024-08-19 14:02:36 +0900

    update

--- 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+export default function GameBoard({onSelectSquare, board}) {
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-const initialGameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null],
-]
-export default function GameBoard({onSelectSquare, activatePlayerSymbol}) {
-	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+	// function handleSelectSquare(rowIndex, colIndex) {
+	// 	setGameBoard((prevGameBoard) => {
+	// 		const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+	// 		updatedBoard[rowIndex][colIndex] = activatePlayerSymbol;
+	// 		return updatedBoard;
+	// 	});
 
-	function handleSelectSquare(rowIndex, colIndex) {
-		setGameBoard((prevGameBoard) => {
-			const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-			updatedBoard[rowIndex][colIndex] = activatePlayerSymbol;
-			return updatedBoard;
-		});
-
-		onSelectSquare();
-	}
+	// 	onSelectSquare();
+	// }
 
 	return (
-		<ol id="game-board">
-			{gameBoard.map((row, rowIndex) => (
-				<li key={rowIndex}>
-					<ol>
-						{row.map((playerSymbol, colIndex) => (
-							<li key={colIndex}>
-								<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
-							</li>
-						))}
-					</ol>
-				</li>
-			))}
-		</ol>
-	);
+        <ol id="game-board">
+            {board.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button
+                                    onClick={() =>
+                                        onSelectSquare(rowIndex, colIndex)
+                                    }
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
 }
