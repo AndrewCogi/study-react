5b7c10ac89a680e856318a0e0bd220c7484c3af5
AndrewCogi <whgustlr0326@gmail.com>
2024-08-19 10:57:40 +0000

    update 04

--- 04-practice-project1/src/App.jsx
+++ 04-practice-project1/src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Result from "./components/Result";
+import UserInput from "./components/UserInput";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+	const [userInput, setUserInput] = useState({
+		initialInvestment: 10000,
+		annualInvestment: 1200,
+		expectedReturn: 6,
+		duration: 10,
+	});
+
+	const inputisValid = userInput.duration > 0;
+
+	function handleChange(inputId, val) {
+		setUserInput((prevUserInput) => {
+			return {
+				...prevUserInput,
+				[inputId]: +val
+			};
+		});
+	}
+
+	return (
+		<>
+			<Header />
+			<UserInput onChange={handleChange} userInput={userInput} />
+			{!inputisValid && <p className="center">Please enter a duration greater than zero.</p>}
+			{inputisValid && <Result input={userInput} />}
+		</>
+	);
 }
 
 export default App
